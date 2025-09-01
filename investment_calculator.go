2d73c45e9837cb870434d00d7033bc7ed19624c7
Roman <60399380+cd-roman@users.noreply.github.com>
2025-09-02 01:01:31 +0200

    Refactor investment calculator: Add formatted string examples

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,28 +1,31 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"math"
-// )
+import (
+	"fmt"
+	"math"
+)
 
-// func main() {
-// 	const inflationRate = 2.5
-// 	var investmentAmount float64
-// 	var years float64
-// 	var expectedReturnRate float64
+func main() {
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
 
-// 	fmt.Print("Enter investment amount: ")
-// 	fmt.Scan(&investmentAmount)
+	fmt.Print("Enter investment amount: ")
+	fmt.Scan(&investmentAmount)
 
-// 	fmt.Print("Enter number of years: ")
-// 	fmt.Scan(&years)
+	fmt.Print("Enter number of years: ")
+	fmt.Scan(&years)
 	
-// 	fmt.Print("Enter expected return rate: ")
-// 	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Enter expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
 
-// 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
-// 	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
 
-// 	fmt.Println(futureValue)
-// 	fmt.Println(futureRealValue)
-// }
+	formattedFutureValue := fmt.Sprintf("Future Value: %.2f\n", futureValue)
+	formattedFutureRealValue := fmt.Sprintf("Future Real Value: %.2f\n", futureRealValue)
+
+	fmt.Print(formattedFutureValue)
+	fmt.Print(formattedFutureRealValue)
+}
--- profit_calculator.go
+++ profit_calculator.go
@@ -1,26 +1,26 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func main() {
-	var revenue, expenses, taxRate float64
+// func main() {
+// 	var revenue, expenses, taxRate float64
 
-	fmt.Print("Enter your revenue: ")
-	fmt.Scan(&revenue)
+// 	fmt.Print("Enter your revenue: ")
+// 	fmt.Scan(&revenue)
 
-	fmt.Print("Enter your expenses: ")
-	fmt.Scan(&expenses)
+// 	fmt.Print("Enter your expenses: ")
+// 	fmt.Scan(&expenses)
 
-	fmt.Print("Enter your tax rate: ")
-	fmt.Scan(&taxRate)
+// 	fmt.Print("Enter your tax rate: ")
+// 	fmt.Scan(&taxRate)
 
-	earningsBeforeTax := revenue - expenses
-	profit := earningsBeforeTax * (1 - taxRate/100)
-	earningsRatio := earningsBeforeTax / profit
+// 	earningsBeforeTax := revenue - expenses
+// 	profit := earningsBeforeTax * (1 - taxRate/100)
+// 	earningsRatio := earningsBeforeTax / profit
 
-	fmt.Println("Your earnings before tax:", earningsBeforeTax)
-	fmt.Println("Your profit after tax:", profit)
-	fmt.Println("Your earnings ratio:", earningsRatio)
-}
+// 	fmt.Println("Your earnings before tax:", earningsBeforeTax)
+// 	fmt.Println("Your profit after tax:", profit)
+// 	fmt.Println("Your earnings ratio:", earningsRatio)
+// }
