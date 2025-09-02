a794a371c43321b3b9fb148602d4935b32780b5f
Roman <60399380+cd-roman@users.noreply.github.com>
2025-09-02 17:46:27 +0200

    Implement balance management: read and write account balance from/to file

--- bank.go
+++ bank.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
+const accountBalanceFile = "balance.txt"
+
+func getBalanceFromFile() float64 {
+	data, _ := os.ReadFile(accountBalanceFile)
+	balanceText := string(data)
+	balance, _ := strconv.ParseFloat(balanceText, 64)
+
+	return balance
+}
+
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile("balance.txt", []byte(balanceText), 0644)
+}
+
 func main() {
-	var accountBalance float64 = 1000.00
+	var accountBalance float64 = getBalanceFromFile()
 
 	fmt.Println("Welcome to Go Bank!")
 
@@ -19,23 +36,22 @@ func main() {
 		var choice int
 		fmt.Print("Your choice: ")
 		fmt.Scan(&choice)
-	
-		if choice == 1 {
+
+		switch choice {
+		case 1:
 			checkBalance(accountBalance)
-		} else if choice == 2 {
+		case 2:
 			accountBalance = depositMoney(accountBalance)
-		} else if choice == 3 {
+		case 3:
 			accountBalance = withdrawMoney(accountBalance)
-		} else if choice == 4 {
+		case 4:
 			fmt.Println("Exiting...")
-			break
-		} else {
+			fmt.Println("Thank you for using Go Bank!")
+			return
+		default:
 			fmt.Println("Invalid choice. Please try again.")
 		}
 	}
-
-	fmt.Println("Thank you for using Go Bank!")
-
 }
 
 func checkBalance(balance float64) {
@@ -55,6 +71,7 @@ func depositMoney(balance float64) float64 {
 	balance += amount
 	fmt.Printf("You have deposited: %.2f\n", amount)
 	fmt.Printf("Your new balance is: %.2f\n", balance)
+	writeBalanceToFile(balance)
 	return balance
 }
 
@@ -72,5 +89,6 @@ func withdrawMoney(balance float64) float64 {
 	}
 
 	fmt.Printf("Your new balance is: %.2f\n", balance)
+	writeBalanceToFile(balance)
 	return balance
 }
